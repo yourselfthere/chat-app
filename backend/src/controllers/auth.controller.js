88cbfa699b49cfcd9d779313e8c10c5db9c28f93
Navneet Singh <nsd02rtk@gmail.com>
2025-04-10 03:54:11 +0530

    check function

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -76,8 +76,28 @@ export const logout = (req, res) => {
 };
 export const updateProfile = async (req, res) => {
   try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile pic is required" });
+    }
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
   } catch (error) {
     console.log("error in logout controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller", error.message);
+    res.status(500).json({ message: "Internal Server error" });
   }
 };
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { login, signup, logout } from "../controllers/auth.controller.js";
+import {
+  login,
+  signup,
+  logout,
+  updateProfile,
+  checkAuth,
+} from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
@@ -7,4 +13,8 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.put("/update-profile", protectRoute, updateProfile); //protectRoute is a middleware to check when user is authenticated
+
+router.get("/check", protectRoute, checkAuth);
+//will be calling the check whenever we refresh maybe something more
+
 export default router;
