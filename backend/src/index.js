9f1c6d0459ab3bbdc4b3d709a84a7bc0e27b7e71
Navneet Singh <nsd02rtk@gmail.com>
2025-04-12 02:47:09 +0530

    messages 120

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,10 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js"; // Need to add `.js` since using ES Modules
+import messaegRoutes from "./routes/message.route.js"; // Need to add `.js` since using ES Modules
+
 import cookieParser from "cookie-parser";
+
 // Load environment variables from .env file
 dotenv.config();
 
@@ -18,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 // Routes
 app.use("/api/auth", authRoutes); // Mount authentication routes
-
+app.use("/api/message", messaegRoutes);
 // Start the server and connect to the database
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
