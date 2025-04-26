2a5b2fdc15870c76642f6515738175642799ce07
Navneet Singh <nsd02rtk@gmail.com>
2025-04-26 17:50:33 +0530

    247 homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { selectedUser } = useChatStore();
+  return (
+    <div className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            {/* <Sidebar /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -21,4 +21,18 @@ export const useChatStore = create((set) => ({
       set({ isUsersLoading: false });
     }
   },
+  getMessages: async (userId) => {
+    // we would need to tell the userId which message we want
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      const message =
+        error.response?.data?.message || "Something went wrong in chatStore";
+      toast.error(message);
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
 }));
