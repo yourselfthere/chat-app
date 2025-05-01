462cbe9ce981e132bf63a3658c3d24a243d43986
Navneet Singh <nsd02rtk@gmail.com>
2025-05-01 15:03:49 +0530

    320 messaegs sendMessages

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
 export const getUsersForSidebar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
