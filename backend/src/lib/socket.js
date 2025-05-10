ffcd6deddec951d85ed7a2a42499af60798c7c32
Navneet Singh <nsd02rtk@gmail.com>
2025-05-10 11:29:02 +0530

    350 socket.io

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -10,4 +10,17 @@ const io = new Server(server, {
     origin: ["http://localhost:5173"],
   },
 });
+//used ot store online users
+const userSocketMap = {}; //{userId: socket.id}
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 export { io, app, server };
